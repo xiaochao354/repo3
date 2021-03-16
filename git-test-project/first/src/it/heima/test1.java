2d802485cd267348a48d5c6123ccc9205a55dac4
yang <1419273071@qq.com>
2021-03-16 22:09:16 +0800

    第一次提交

--- git-test-project/first/src/it/heima/test1.java
+++ git-test-project/first/src/it/heima/test1.java
@@ -4,4 +4,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("diyi");
     }
+    private void tes(){
+        System.out.println("slfjas");
+    }
 }
