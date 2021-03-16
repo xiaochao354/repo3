8672e342b0e846aaa66a8169eb84940d00e958ba
yang <1419273071@qq.com>
2021-03-16 22:07:13 +0800

    第一次提交

---
+++ git-test-project/first/src/it/heima/test1.java
@@ -0,0 +1,7 @@
+package it.heima;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("diyi");
+    }
+}
---
+++ git-test-project/first/src/it/heima/test2.java
@@ -0,0 +1,7 @@
+package it.heima;
+
+public class test2 {
+    public void test(){
+        System.out.println("21321");
+    }
+}
